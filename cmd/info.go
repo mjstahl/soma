60ae04d4e8270e13e702f47b18a13359f6e22357
Mark Stahl <mark@work.local>
2014-12-12 20:17:29 -0500

    Updated the year displayed by the info command

--- cmd/info.go
+++ cmd/info.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 )
 
-var InfoUsage = `Usage: 
+var InfoUsage = `Usage:
     soma info
 
-    Displays information about the Social Machines 
+    Displays information about the Social Machines
     language runtime.  If in a project directory
-    the output includes information specific to 
+    the output includes information specific to
     the project.
 
 Example:
     $ soma info
-    Social Machines vX.Y.Z
-    Copyright (C) 2013 Mark Stahl
+    Social Machines vA.B.C
+    Copyright (C) YYYY Mark Stahl
 
     Portions of this source code are:
-    Copyright (C) 2012 The Go Authors
+    Copyright (C) YYYY The Go Authors
 `
 
 func RuntimeInfo(ver string) {
 	fmt.Printf("Social Machines v%s\n", ver)
-	fmt.Printf("Copyright (C) 2013 Mark Stahl\n")
+	fmt.Printf("Copyright (C) 2014 Mark Stahl\n")
 	fmt.Printf("\n")
 	fmt.Printf("Portions of this source code are:\n")
 	fmt.Printf("Copyright (C) 2012 The Go Authors\n")
